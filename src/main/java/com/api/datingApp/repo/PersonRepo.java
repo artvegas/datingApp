7bd04212ba06c88cbcd7c4ca2bea4dbca00618e8
Aritra Nirmal <anirmal@cs.stonybrook.edu>
2018-03-22 19:03:21 -0400

    Revert "original project dump"
    This reverts commit ce2e58e8116d0e541acd332210530089113a6859.

--- src/main/java/com/api/datingApp/repo/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.api.datingApp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.datingApp.model.Person;
-
-public interface PersonRepo extends JpaRepository<Person, String> {
-
-}
